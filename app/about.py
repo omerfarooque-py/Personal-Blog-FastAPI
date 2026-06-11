4150144603a93490a4aa4c0f7582ae785558d53d
omerFarooque <omer.farooque@yahoo.com>
2026-06-11 15:06:36 +0500

    IMPROVEMENTS

--- app/about.py
+++ app/about.py
@@ -7,11 +7,11 @@ def render_about_tab():
     st.markdown("---")
     col_img, col_txt = st.columns([1, 2])
     with col_img:
-        st.image("https://avatars.githubusercontent.com/u/220927412?v=4", caption="Umer Farooque")
+        st.image("https://avatars.githubusercontent.com/u/220927412?v=4", caption="Omer Farooque")
         st.markdown("🔗 **Profiles:** [GitHub](https://github.com/omerfarooque-py) | [LinkedIn](https://www.linkedin.com/in/omer-farooque-b04825346/)")
     with col_txt:
        st.markdown("""
-        ### Umer Farooque
+        ### Omer Farooque
         **Backend Developer | Aspiring AI Engineer**
 
         I enjoy building backend systems with Python, FastAPI, and PostgreSQL, focusing on clean APIs, authentication, database design, and scalable application architecture.
